0f042f3aa03f474df6b175d174f67854697623d3
keprom <kokshetau.energo.prom@mail.ru>
2018-12-05 11:01:16 +0600

    fix date in adding tariff value

--- system/application/controllers/billing.php
+++ system/application/controllers/billing.php
@@ -3587,7 +3587,7 @@ where firm_id={$this->uri->segment(3)} and data_finish is null";
         $this->db->where("period_id = industry.current_period_id()");
         $prev_values = $this->db->get("industry.tariff_current_value")->row();
 
-        if ($data <= $prev_values->tariff_data) {
+        if ($data < $prev_values->tariff_data) {
             die("date error");
         }
 
